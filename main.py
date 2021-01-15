5f85250a160e5dc42ae1afce8b30e3d1798c74b1
Oliver Cordingley <olivercordingley@me.com>
2021-01-15 15:40:53 +0000

    added list search functionality

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ Works by validating the email against a series of regular expressions
 # import libraries
 import argparse
 import re   # regular expression library
+import time # to time the output
 
 # argparse package allows terminal arguments to be parsed neatly, and makes input less of a drag to obtain
 parser = argparse.ArgumentParser(description='Validates email addresses.') # Initialise our argument parser
@@ -70,7 +71,53 @@ def validateEmail(address, quiet=False):
 
         return f"{True}"
 
-if __name__ == '__main__':
-    # for now we will just check one email address without input from the user.
+def validateList(list):
+    timestart = time.time()
+    out = []
+    data = ''
+    counter = 0
 
-    print(validateEmail("hello23@iclouw24-d.cds", False))
+    # open the file listed in the -L arg
+    # Do this in a try-catch block incase a user provides the path to a file that does not exist
+    try:
+        with open(args.list, 'r') as f:
+            # read and split up the data of the file by newlines
+            # (yes, i could use f.readline(), but I can't be bothered to strip the \n from every line that I read.)
+            data = f.read()
+    # If the file cannot be found, this is the exception that open() will throw. No exit code 1s on my watch 😎
+    except FileNotFoundError:
+        print('The file does not exist.')
+
+    data = [x.strip() for x in data.split('\n') if x.strip()]
+    if (len(data) == 0):
+        out.append("There was no data to analyse.")
+    else:
+        out.append(f'Validating {len(data)} email addresses.\n')
+
+        for line in data:
+            if (line.strip() == ''): continue
+            
+            isValid = validateEmail(line, args.quiet)
+
+            if (isValid != "True") :
+                out.append(f"[FAIL] {line} | {isValid}")
+            else:
+                out.append(f"[PASS] {line} | {isValid}")
+                counter += 1
+
+        timeend = time.time()
+        out.append(f'\n\n=====> COMPLETE [{counter}/{len(data)}] ({round((timeend - timestart) * 1000, 1)}ms elapsed) <=====')
+
+        return '\n'.join(out)
+            
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    if (args.list != None):
+        print(validateList(args.list))
+    elif (args.address != None):
+        print(f"[*] {args.address} | {validateEmail(args.address, args.quiet)}")
+    else:
+        print('For help using the command, use the --help flag.')
